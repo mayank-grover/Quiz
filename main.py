5a9823a033d16f27e0e4845266b8dfbe7813096b
mayank-grover <90093490+mayank-grover@users.noreply.github.com>
2022-01-23 17:51:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# person1 = Person("Mayank", 16)
+# print(person1.age)
+
+optionList = []
+
+class Question:
+  def __init__(self, question, options, answer):
+    self.question = question
+    self.options = options
+    self.answer = answer
+
