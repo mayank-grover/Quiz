fdef02b1d8ad17481005eb03e0a171620838315e
mayank-grover <90093490+mayank-grover@users.noreply.github.com>
2022-01-23 18:46:10 +0000

    added askquestion func with check answer as well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 
 optionList = []
 
@@ -7,4 +7,20 @@ class Question:
     self.question = question
     self.options = options
     self.answer = answer
+  def askQuestion(self):
+    print(self.question + "\n")
+    random.shuffle(self.options)
+    for i in self.options:
+      print(i)
+  
+    userChoice = input("\n" + "What is your answer?")
 
+    if userChoice == self.answer:
+      print("Correct!")
+      return True
+    else:
+      print("Incorrect")
+      return False
+
+x = Question("What is your favourite food?", ["burger", "noodles", "brownie", "pizza"], "pizza")
+x.askQuestion()
